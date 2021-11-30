3a8a84b9fda1c6694849337a11879096ae4f5933
Sanu Kumar <94282700+Sanu15kumar1999@users.noreply.github.com>
2021-11-30 21:57:35 +0530

    Create user_utils.h

---
+++ 03_Implementation/inc/user_utils.h
@@ -0,0 +1,15 @@
+#ifndef __USER_UTILS_H__
+#define __USER_UTILS_H__
+
+#include "project_config.h"
+#include <util/delay.h>
+
+/**
+ * @brief Function to generate delay in micro seconds
+ * 
+ * @param[in] delay_time Delay in Micro seconds
+ * 
+ */
+void delay_ms(uint32_t delay_time);
+
+#endif /* __USER_UTILS_H__ */
