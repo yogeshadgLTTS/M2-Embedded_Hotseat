b1e6589b4199f985b5366f9374f29319517fe530
Sanu Kumar <94282700+Sanu15kumar1999@users.noreply.github.com>
2021-11-30 22:16:29 +0530

    Create main.c

---
+++ 03_Implementation/main.c
@@ -0,0 +1,35 @@
+#include "activity1.h"
+#include "activity2.h"
+#include "activity3.h"
+
+int main(void)
+{
+    uint16_t temp;
+    
+    while(1)
+    {
+	 
+        if(activity1()==1) 
+        {
+            /* Activity 1 Toggling Led*/ 
+            led_on();
+		
+	    /* Activity_2 related to ADC value*/
+            temp=activity2();
+		
+	    /* Activity 3 Pulse Width Modulation according to its ADC value*/
+            activity3(temp); 
+
+	   
+            
+
+        }
+        else  
+        {
+            led_off();
+		    _delay_ms(2000);
+        }
+
+    }
+    return 0;
+}
