14024774ee3aef9bc53b699aee5654ce05af056e
Sanu Kumar <94282700+Sanu15kumar1999@users.noreply.github.com>
2021-11-30 21:55:54 +0530

    Create project_config.h

---
+++ 03_Implementation/inc/project_config.h
@@ -0,0 +1,14 @@
+#ifndef __PROJECT_CONFIG_H__
+#define __PROJECT_CONFIG_H__
+
+/**
+ * Macro Definitions
+ */
+
+#define F_CPU 16000000UL 	/**< Clock Frequency of MCU is 16 MHz */
+
+#define LED_PORT (PORTB)    /**< LED Port Number */
+#define LED_PIN  (PORTB0)   /**< LED Pin number  */
+
+
+#endif /* __PROJECT_CONFIG_H__ */
