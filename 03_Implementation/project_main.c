9679394fa981322b0c8dacb66d90d3c97b6f72a3
Sanu Kumar <94282700+Sanu15kumar1999@users.noreply.github.com>
2021-11-30 22:19:23 +0530

    Create project_main.c

---
+++ 03_Implementation/project_main.c
@@ -0,0 +1,38 @@
+
+#include "project_config.h"
+
+#include "user_utils.h"
+#include "blinky.h"
+
+/**
+ * @brief Initialize all the Peripherals and pin configurations
+ * 
+ */
+void peripheral_init(void)
+{
+	/* Configure LED Pin */
+	DDRB |= (1 << DDB0);
+}
+
+void change_led_state(uint8_t state)
+{
+	LED_PORT = (state << LED_PIN);
+}
+
+
+
+int main(void)
+{
+	/* Initialize Peripherals */
+	peripheral_init();
+
+	for(;;)
+	{
+        change_led_state(LED_ON);
+		delay_ms(LED_ON_TIME);
+		
+        change_led_state(LED_OFF);
+		delay_ms(LED_OFF_TIME);	
+	}
+	return 0;
+}
